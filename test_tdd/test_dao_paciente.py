fbf45cc66286a2d8b00c33136c4fb99dccacb600
Lautaro Villafañe <lautarovillafane98@gmail.com>
2025-12-17 12:22:44 -0300

    Diagrama de clases y modelo relacional creado previo al proyecto pero se agrega recien

--- test_tdd/test_dao_paciente.py
+++ test_tdd/test_dao_paciente.py
@@ -5,3 +5,6 @@ from dao.interfaz_dao.interfaz_paciente_dao import PacienteDAOInterfaz
 from dominio.paciente import Paciente
 from mysql.connector import errorcode
 
+@pytest.fixture(scope="module")
+def conn():
+    db_conn = DBConn("test_config.ini")
